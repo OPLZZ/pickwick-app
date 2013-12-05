3c7a785b887d2b52898d344f432fbd8d443c87b4
Ondřej Bartas <ondrej@bartas.cz>
2013-12-05 22:00:00 +0100

    [ADD] INIT

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DamePraciApp::Application.config.secret_key_base = 'fb8543c632c30b85297886238d1ec4ab2f09e1c1d64f28140f35710bf8275d25cfc62a816f674007835de90df160af5aa983febfc21bd3593fac44ad76c8ee27'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DamePraciApp::Application.config.session_store :cookie_store, key: '_dame_praci_app_session'
