ae0c2e2226cf99868b8a54487179174d88d51c5a
Ondřej Bartas <ondrej@bartas.cz>
2014-04-08 12:04:13 +0200

    [ADD] Sign out

--- app/assets/javascripts/sign_in.js
+++ app/assets/javascripts/sign_in.js
@@ -4,10 +4,18 @@ hello.on('auth.login', function(auth){
     hello( auth.network ).api( '/me' ).success(function(r){
       Ember.run(function(){
         controller = PickwickApp.__container__.lookup('controller:application');
-        user = PickwickApp.User.create({'name': r.name, 'id': r.id, 'thumbnail': r.thumbnail, 'obj': r});
+        user = PickwickApp.User.create({'name': r.name, 'id': r.id, 'thumbnail': r.thumbnail, 'obj': r, 'auth': auth});
         controller.set('user', user)
       })
     });
+  });
+
+hello.on('auth.logout', function(auth){
+    // call user information, for the given network
+    Ember.run(function(){
+      controller = PickwickApp.__container__.lookup('controller:application');
+      controller.set('user', undefined)
+    })
   });
 
 hello.init({ 
