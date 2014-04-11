48d4b5a755cfaaca3f2d63530983528ac4c0602a
Ondřej Bartas <ondrej@bartas.cz>
2014-04-11 12:15:38 +0200

    [ĄDD] SERVER SIDE Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 PickwickApp::Application.routes.draw do
 
-  resource 'users', only:[:create, :show] do
-    resource 'job_postings'
+  match '/admin' => 'job_postings#admin_index', via: [:get], as: 'admin_index'
+  match '/admin/:id/valid' => 'job_postings#admin_valid', via: [:get,:post], as: 'admin_valid'
+  match '/admin/:id/invalid' => 'job_postings#admin_invalid', via: [:get,:post], as: 'admin_invalid'
+
+  resources 'users', only:[:create, :show] do
+    resources 'job_postings'
   end
 
   root "root#index"
 
   match "*path" => "root#index", via: [:get, :post]
+
 
 end
