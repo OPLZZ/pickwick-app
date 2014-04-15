d66aed8a627333a7a781e150df52da813cb16fed
Ondřej Bartas <ondrej@bartas.cz>
2014-04-15 10:31:26 +0200

    [FIX] User Controller - multiple renders and redirects in one request (needed 'and return')

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
 
   def authorize_application_token
     if request.headers['Application-Token'] != "59a3b1a51c80c8db71c9a881d8b23c6e2b41727c"
-      render json: {error: 'UNAUTHORIZED'}, status: 401
+      render json: {error: 'UNAUTHORIZED'}, status: 401 and return
     end
   end
 
@@ -21,21 +21,21 @@ class UsersController < ApplicationController
       create_params[:system_id] = fb_user.identifier
       create_params[:name] = fb_user.name
     rescue Exception => e
-      render json: {error: 'INVALID LOGIN', message: e.message}, status: 400
+      render json: {error: 'INVALID LOGIN', message: e.message}, status: 400 and return
     end
 
     if user_params[:token] && @user = User.where(system_id: create_params[:system_id]).first
       #update token if needed
       @user.token = user_params[:token] if @user.token != user_params[:token]
       @user.save
-      render json: {user: @user}, status: 200
+      render json: {user: @user}, status: 200 and return
     else
       @user = User.new(create_params)
 
       if @user.save
-        render json: {user: @user}, status: 201
+        render json: {user: @user}, status: 201 and return
       else
-        render json: {error: @user.errors.full_messages}, status: 400
+        render json: {error: @user.errors.full_messages}, status: 400 and return
       end
     end
   end
